0cfd9cdb854c14e5c1a46ad80b367818a70596f8
root <root1234>
2024-11-21 02:26:12 +0800

    test OverlappingFileLockException

--- ms-ping/src/test/groovy/com/yuwangyx/ping/config/PingScheduledTaskSpec.groovy
+++ ms-ping/src/test/groovy/com/yuwangyx/ping/config/PingScheduledTaskSpec.groovy
@@ -1,10 +1,14 @@
-package com.yuwangyx.ping.service.impl
+package com.yuwangyx.ping.config
 
-import com.yuwangyx.ping.config.PingScheduledTask
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import spock.lang.Specification
+
+import java.nio.channels.FileChannel
+import java.nio.channels.FileLock
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "ping.task.scheduling.enabled=true")
 @ActiveProfiles("test")
@@ -16,6 +20,12 @@ class PingScheduledTaskSpec extends Specification {
     def "test scheduledPing"() {
 
         when:
+        // make OverlappingFileLockException for RateLimiter catch
+        FileChannel channel = FileChannel.open(Paths.get("E:\\workspace\\pingpong\\rate_limit_temp.lock"), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE)
+        FileLock lock = channel.lock()
+        sleep(1000)
+        lock.release()
+
         pingScheduledTask.scheduledPing()
 
         then:
